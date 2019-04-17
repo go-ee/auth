8c335c213ac8b26f7dfa16e6745815b57948ba0d
Eugen Eisler <eoeisler@gmail.com>
2019-04-17 11:46:10 +0200

    Update

---
+++ app/Auth.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"errors"
+
+	"github.com/go-ee/auth"
+
+	"github.com/go-ee/utils/crypt"
+	"github.com/go-ee/utils/eh/app"
+	"github.com/go-ee/utils/net"
+	"github.com/looplab/eventhorizon"
+)
+
+type Auth struct {
+	*app.AppBase
+}
+
+func NewAuth(appBase *app.AppBase) *Auth {
+	appBase.ProductName = "Auth"
+	return &Auth{AppBase: appBase}
+}
+
+func (o *Auth) Start() {
+
+	authEngine := auth.NewAuthEventhorizonInitializer(o.EventStore, o.EventBus, o.CommandBus, o.ReadRepos)
+	authEngine.Setup()
+	authEngine.ActivatePasswordEncryption()
+
+	authRouter := auth.NewAuthRouter("", o.Ctx, o.CommandBus, o.ReadRepos)
+	authRouter.Setup(o.Router)
+
+	if o.Secure {
+		o.Jwt = o.initJwtController(authRouter.AccountRouter.QueryHandler.QueryRepository)
+	}
+
+	o.StartServer()
+}
+
+func (o *Auth) initJwtController(accounts *auth.AccountQueryRepository) (ret *net.JwtController) {
+	//TODO use appName, provide help how to generate RSA files first
+	return net.NewJwtControllerApp("app",
+		func(credentials net.UserCredentials) (ret interface{}, err error) {
+			var account *auth.Account
+			if account, err = accounts.FindById(eventhorizon.UUID(credentials.Username)); err == nil {
+				if !crypt.HashAndEquals(credentials.Password, account.Password) {
+					err = errors.New("password mismatch")
+				} else {
+					ret = account
+				}
+			}
+			return
+		})
+}
---
+++ example/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-ee/auth/app"
+	"github.com/go-ee/utils/eh/app/memory"
+	"github.com/go-ee/utils/eh/app/mongo"
+	"github.com/go-ee/utils/lg"
+	"github.com/urfave/cli"
+)
+
+var Log = lg.NewLogger("CGT ")
+
+func main() {
+
+	var flag_secure = "secure"
+	var flag_name = "name"
+
+	name := "Auth"
+
+	runner := cli.NewApp()
+	runner.Usage = name
+	runner.Version = "1.0"
+	runner.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "name, n",
+			Usage: "name of the scool, used for backend data",
+			Value: "Auth",
+		},
+		cli.BoolFlag{
+			Name:  "secure, s",
+			Usage: "activate secure mode",
+		},
+	}
+
+	flag_url := "url"
+	runner.Commands = []cli.Command{
+		{
+			Name:  "mongo",
+			Usage: "Start server with MongoDB backend",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  flag_url,
+					Usage: "url of the MongoDB instance",
+					Value: "localhost",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				Auth := app.NewAuth(mongo.NewAppMongo(name, c.GlobalString(flag_name), c.GlobalBool(flag_secure), c.String(flag_url)))
+				Auth.Start()
+				return
+			},
+		}, {
+			Name:  "memory",
+			Usage: "Start server with memory backend",
+			Action: func(c *cli.Context) (err error) {
+				Auth := app.NewAuth(memory.NewAppMemory(name, c.GlobalString(flag_name), c.GlobalBool(flag_secure)))
+				Auth.Start()
+				return
+			},
+		},
+	}
+
+	if err := runner.Run(os.Args); err != nil {
+		Log.Err("%v", err)
+	}
+}
